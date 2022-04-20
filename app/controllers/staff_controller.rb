50448d702de474db29228c6e39945111569da971
FlowDrucka64 <flow.drucka64@gmx.at>
2022-04-20 20:31:59 +0200

    Staff search (api pull + result pages)

--- app/controllers/staff_controller.rb
+++ app/controllers/staff_controller.rb
@@ -1,12 +1,26 @@
-class StaffController < ApplicationController
+class StaffController < SearchController
 
-  #TODO: actual search
-  #(currently just filling with something to test partials)
   def search
-    if params[:search]
-      @results = current_user.staff_favourites_objects
-    else
-      @results = current_user.staff_favourites_objects
+    @results = []
+    if params[:q]
+      @results = api_search(params[:q])
+      @results["result_slice"] = @results["results"].slice(20 * (params[:p].to_i - 1), 20)
     end
   end
+
+  private
+
+  def api_search(search_term)
+    #TODO: cache results?
+    url = BASE_API_URL + STAFF_SEARCH_URI + search_term.to_s + "&max_treffer=60"
+    response = Excon.get(url)
+    return nil if response.status != 200
+    return search_transform(JSON.parse response.body)
+  end
+
+  def search_transform(response)
+    response["page_count"] = (response["results"].length / 20).to_f.ceil.to_i #calculate how much result pages are needed
+    return response
+  end
+
 end
---
+++ config/initializers/api_urls.rb
@@ -0,0 +1,3 @@
+BASE_API_URL = "https://tiss.tuwien.ac.at/api".freeze
+
+STAFF_SEARCH_URI = "/person/v22/psuche?q=".freeze
